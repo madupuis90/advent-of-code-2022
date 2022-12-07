87458570039de29e02dce7e50a8ef406768fb3e7
madupuis90 <ma.dupuis.90@gmail.com>
2022-12-06 22:27:40 -0500

    day3 - part 1

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,29 +1,7 @@
 mod part_1;
 mod part_2;
 
-#[derive(Clone, Copy)]
-enum Result {
-    Lose = -1,
-    Draw = 0,
-    Win = 1,
-}
-impl Result {
-    fn value(&self) -> i32 {
-        match &self {
-            Result::Win => 1,
-            Result::Draw => 0,
-            Result::Lose => -1,
-        }
-    }
-}
-
 fn main() {
-    // println!("{}", part_1::process("input.txt"));
-    // println!("{}", part_2::process("input.txt"));
-    let a = Result::Win;
-    let b = Result::Lose;
-    let c = a as i32 + b as i32;
-    let d: i32 = 0;
-    let e = d as Result;
-    println!("results are a={} and b={} and c={}", a as i32, b as i32, e);
+    println!("{}", part_1::process("input.txt"));
+    // println!("{}", part_2::process("sample.txt"));
 }
--- day3/src/part_1.rs
+++ day3/src/part_1.rs
@@ -1,10 +1,29 @@
 use std::fs::File;
-use std::io::prelude::*;
-use std::io::BufReader;
-use std::vec::Vec;
+use std::io::{BufRead, BufReader};
 
 pub fn process(input_file: &str) -> String {
     let file = File::open(input_file).expect("Input file should be in root directory");
     let reader = BufReader::new(file);
-    String::from("bob")
+
+    let mut priorities: i32 = 0;
+    for line in reader.lines() {
+        let line = line.expect("valid line");
+        let (first_part, second_part) = line.split_at(line.len() / 2);
+        priorities = priorities + compare_parts(first_part, second_part);
+    }
+    String::from(format!("{priorities}"))
+}
+
+fn compare_parts(p1: &str, p2: &str) -> i32 {
+    let mut value: i32 = 0;
+    for c in p1.chars() {
+        if p2.find(c).is_some() {
+            if c.is_uppercase() {
+                value = c as i32 - 38
+            } else {
+                value = c as i32 - 96
+            }
+        }
+    }
+    value
 }
